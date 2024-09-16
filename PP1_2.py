355b0354fea28ff85dc22928d1edb83145f161ed
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-09-16 14:17:20 +0000

    comepleted

--- PP1_2.py
+++ PP1_2.py
@@ -3,18 +3,38 @@
 #Write Assignment code here
 
 def q1():
+    name = Alex
+    print (name)
 
 def q2():
+    three = 3
+    five = 5
+    two = 2
+    print (three + two + five)
 
 def q3():
+    twoOne = 21
+    four = 4
+    print (twoOne // four)
 
 def q4():
+    floatOne = 6.3
+    floatTwo = 3.0
+    print ( floatOne / floatTwo)
 
 def q5():
+    word = "Hello World"
+    word = "Goodbye World"
+    print (word)
 
 def q6():
+    false = True
+    print (false)
 
 def q7():
+    two = 2
+    seven = 7
+    print (seven % two)
 
 q1()
 q2()
